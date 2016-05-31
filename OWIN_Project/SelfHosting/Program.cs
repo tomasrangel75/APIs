4983c12ad1a8182f31efa9e5bdec2c29a10b0a07
Tomas <tomas.rangel75@gmail.com>
2016-05-30 22:22:17 -0300

    20150530 22?10

---
+++ OWIN_Project/SelfHosting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHosting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (Microsoft.Owin.Hosting.WebApp.Start<Startup1>("http://localhost:9000"))
+			{
+				Console.WriteLine("Press [enter] to quit...");
+				Console.ReadLine();
+			}
+		}
+	}
+}
